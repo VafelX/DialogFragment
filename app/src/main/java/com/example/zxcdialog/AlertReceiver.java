888ec7689d476e2c9a70c77d6d43d552c9105274
Vidix <72098622+VafelX@users.noreply.github.com>
2021-05-11 16:03:38 +0300

    dfssdf

---
+++ app/src/main/java/com/example/zxcdialog/AlertReceiver.java
@@ -0,0 +1,20 @@
+package com.example.zxcdialog;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1, nb.build());
+        final Ringtone r = RingtoneManager.getRingtone(context, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
+        r.play();
+    }
+}
